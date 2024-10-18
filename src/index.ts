8fd508ec8c9ef4f9b0c12fdd7b0fcd686f4868c0
Your Name <youremail@domain.com>
2024-10-18 13:27:35 +0900

    hahaha

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,9 @@ import { PrismaClient } from '@prisma/client'
 import { conversionByVariant } from '@prisma/client/sql'
 
 async function main() {
-  const prisma = new PrismaClient()
-
-  const stats = await prisma.$queryRawTyped(conversionByVariant())
-
-  console.log(stats)
+    const prisma = new PrismaClient()
+    const stats = await prisma.$queryRawTyped(conversionByVariant())
+    console.log(stats)
 }
 
 main()
